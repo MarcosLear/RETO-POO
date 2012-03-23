9f625bd9386fe040ee6f873be5b3438771a6e8ca
Alex Segovia <alex.segovias@gmail.com>
2012-03-22 20:04:23 -0500

    test fecha

---
+++ Regalos/test/regalos/FacturaTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regalos;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Monchito
+ */
+public class FacturaTest {
+    
+    public FacturaTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+}
