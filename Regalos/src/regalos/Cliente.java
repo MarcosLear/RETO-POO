9c573c56719471aa518419c34fd6232c39513c1c
Alex Segovia <alex.segovias@gmail.com>
2012-03-22 19:55:27 -0500

    programa de regalos

---
+++ Regalos/src/regalos/Cliente.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regalos;
+
+/**
+ *
+ * @author Monchito
+ */
+public class Cliente {
+
+    private String dni;
+    private String apellido;
+    private String Apellido;
+}
---
+++ Regalos/src/regalos/Factura.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regalos;
+
+/**
+ *
+ * @author Monchito
+ */
+public class Factura {
+    
+    private String codFactura;
+    private double montoTotal;
+    private String fecha;
+    
+}
