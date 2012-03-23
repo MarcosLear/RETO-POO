9134fc2e31a530d250fb53643a081859dd072ad4
Alex Segovia <alex.segovias@gmail.com>
2012-03-22 20:33:14 -0500

    test fecha

--- Regalos/src/regalos/Cliente.java
+++ Regalos/src/regalos/Cliente.java
@@ -11,6 +11,48 @@ package regalos;
 public class Cliente {
 
     private String dni;
+    private String nombre;
     private String apellido;
-    private String Apellido;
+    private String tipoCliente;
+
+    public Cliente(String dni, String nombre, String apellido, String tipoCliente) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.tipoCliente = tipoCliente;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+    
+    
 }
